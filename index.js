7a61ba62c632460de4979a5279580163c8f02c78
Jul Jen <juliajen.prog@gmail.com>
2018-09-04 17:26:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,4 +3,5 @@
 const drivers = ["Milo", "Otis", "Garfield"];
 
 // appends a driver to the end of the drivers array
-const destructivelyAppendDriver = drivers.push('Ralph');
+const name = ["Milo", "Otis", "Garfield"];
+const destructivelyAppendDriver = name.push('Ralph');
